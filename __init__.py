01d104eb88c5d2e4fb94546477e403d286404ab2
Omarlakany <omar_lakany@hotmail.com>
2017-11-19 03:36:38 -0500

    Working model with json

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
-print(5)
+import urllib
+
+i=1
+htmltext = urllib.urlopen("http://fantasy.premierleague.com/web/api/elements/" + str(i) + "/")
+
+myfile = open("player_history.txt", "w")
+myfile.close()
+#   urllib.urlopen("http://fantasy.premierleague.com/web/api/elements/" + str(i) + "/")
+ #   json.load(htlmtext)
+print
+
+#for i in range(1,1000):
+ #   urllib.urlopen("http://fantasy.premierleague.com/web/api/elements/" + str(i) + "/")
+ #   json.load(htlmtext)
